0931aae572c1542252c4863e084711a8eaf9b717
NetoPedro <pedroneto_09@hotmail.com>
2019-12-05 01:10:34 +0200

    Modiefied partitions

--- partitioning.py
+++ partitioning.py
@@ -1,24 +1,58 @@
 import networkx as nx
 import scipy.sparse as sparse
 import time
+import sklearn.cluster as cluster
+import numpy as np
+import networkx.algorithms as algo
+import networkx.linalg.algebraicconnectivity as  algebraicconnectivity
 start_time = time.time()
 
 
-G=nx.read_edgelist("graphs_processed/Oregon-1.txt")
+FILENAME = "roadNet-CA.txt"
+first_line = []
+with open("graphs_processed/" + FILENAME) as f:
+    first_line = f.readline()
 
-def find_laplacian(nodelist):
+line = first_line
+first_line = line.split()
+k = first_line[4]
 
-    if nodelist is None:
-        nodelist = list(G)
-    A = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight=None)
-    n, m = A.shape
-    diags = A.sum(axis=1)
-    D = sparse.spdiags(diags.flatten(), [0], m, n)
-    return D - A
 
-laplacian = find_laplacian(G.nodes)
-laplacian = laplacian.asfptype()
-eigval, eigvec = sparse.linalg.eigsh(laplacian, 2, sigma=0, which='LM')
-print(eigval)
-print("ok")
+
+
+
+start_time = time.time()
+
+G=nx.read_edgelist("graphs_processed/"+FILENAME)
+
+vec = algebraicconnectivity.fiedler_vector(G,method="tracemin_lu")
+vec = np.asarray(vec).reshape(-1,1)
+clusters = cluster.KMeans(int(k)).fit_predict(vec)
+
+
+cost = 0
+nodes = np.asarray(list(G.nodes._nodes.keys()))
+
+
+for i in range(int(k)):
+    size = sum(clusters == i)
+    print(size)
+    cost +=  algo.cut_size(G,nodes[clusters == i])/size
+
+print("Cost: ", cost)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+f= open("results/"+FILENAME,"w+")
+
+
+sort_ix = np.argsort(nodes.astype(int))
+
+nodes = nodes[sort_ix]
+
+clusters = clusters[sort_ix]
+f.write(line)
+i = 0
+for node in nodes :
+    f.write(str(node) + " " +  str(clusters[i]) + "\n")
+    i = i+1
+f.close()
